dfc1caccba1a49c81c3a3dba139d4e43c1e2a1f9
Zhannator <zhp3@pitt.edu>
2018-10-28 22:01:20 -0400

    draft 1

--- Linear_SVM_for_Facial_Recognition/svm_main.py
+++ Linear_SVM_for_Facial_Recognition/svm_main.py
@@ -1,13 +1,38 @@
 # Public libraries
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
+	
+	# ----------------------------------------------
+	# TRAINING
+	# ----------------------------------------------
+	
 	# Iterate through facial images
 	for i in range(1, 41, 1):
 		for j in range(1, 11, 1):
-			img = cv2.imread("data/s" + str(i) + "/" + str(j) + ".pgm")
+			# Read image
+			img = cv2.imread("data/s" + str(i) + "/" + str(j) + ".pgm", 0)
 			rows, columns = img.shape
+			# Detect face
+			face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+			faces = face_detector.detectMultiScale(img, scaleFactor = 1.3, minNeighbors = 5)
+			if (len(faces) == 0):
+				print "Error: no face identified in data/s" + str(i) + "/" + str(j) + ".pgm"
+				return -1
+			# Each image contains only 1 face
+			x, y, w, h = faces[0]
+			# Save face img
+			cv2.imwrite("faces/s" + str(i) + "/" + str(j) + ".pgm", img[y:y+w, x:x+h])
+			# Extract principal components
+		return	
 			
+	
+	# Face detection
+	# Freature extraction
+	# Comparison
+	# Face recognition
+		
 if __name__ == "__main__":
 	main()
