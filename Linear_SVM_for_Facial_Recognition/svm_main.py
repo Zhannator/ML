0c33f955baaeb340e6ec7289e043cfeb378c8b09
Zhanneta Plokhovska <zhp3@pitt.edu>
2018-10-29 22:44:04 -0400

    Draft 2

--- Linear_SVM_for_Facial_Recognition/svm_main.py
+++ Linear_SVM_for_Facial_Recognition/svm_main.py
@@ -2,37 +2,53 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from cvxopt import matrix, solvers
 
 def main():
 	
 	# ----------------------------------------------
-	# TRAINING
+	# FEATURE EXTRACTION - PCA
 	# ----------------------------------------------
 	
+	img_height = 112
+	img_width = 92
+	num_of_pixes = img_height * img_width
+	num_of_people = 40
+	num_of_faces_per_person = 10
+	num_of_faces = num_of_people * num_of_faces_per_person
+	Y = [1, -1] # labels
+	X = [] # data matrix
+	
 	# Iterate through facial images
-	for i in range(1, 41, 1):
-		for j in range(1, 11, 1):
+	for i in range(1, num_of_people + 1, 1):
+		for j in range(1, num_of_faces_per_person + 1, 1):
 			# Read image
 			img = cv2.imread("data/s" + str(i) + "/" + str(j) + ".pgm", 0)
 			rows, columns = img.shape
-			# Detect face
-			face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-			faces = face_detector.detectMultiScale(img, scaleFactor = 1.3, minNeighbors = 5)
-			if (len(faces) == 0):
-				print "Error: no face identified in data/s" + str(i) + "/" + str(j) + ".pgm"
-				return -1
-			# Each image contains only 1 face
-			x, y, w, h = faces[0]
-			# Save face img
-			cv2.imwrite("faces/s" + str(i) + "/" + str(j) + ".pgm", img[y:y+w, x:x+h])
-			# Extract principal components
-		return	
-			
+			# 2D image matrix to 1D row vector
+			img = (np.array(img)).flatten()
+			X.append(img)
 	
-	# Face detection
-	# Freature extraction
-	# Comparison
-	# Face recognition
+	# Calculate average / mean face
+	x_mean = (np.array(X)).mean(0)
+	cv2.imwrite('test.png', x_mean.reshape(img_height, img_width))
+	x_mean_matrix = np.array([x_mean] * num_of_faces)
+	x_mean_matrix = np.transpose(x_mean_matrix)
+	# Each face image is a column vector
+	X = np.transpose(X)
+	# Subtract mean x from each column vector x^n in X
+	F = np.subtract(X, x_mean_matrix)
+	# Calculate eigenvalues and eigenvectors - if v is eigenvector of L then Av is eigenvector of S (where S = AAt)
+	A = np.matmul(F, np.transpose(F))
+	L = np.matmul(np.transpose(A), A)
+	eigenvalues, eigenvectors = np.linalg.eig(L)
+
+	# Solve quadratic
+	alfa = cvxopt.solvers.qp(cvxopt.matrix(X), cvxopt.matrix(Y))
+	print alfa
+
+	
+
 		
 if __name__ == "__main__":
 	main()
