ea6e76f68fa94add44f156fc5439ce5b2b578427
Zhanneta Plokhovska <zhp3@pitt.edu>
2018-10-31 10:01:44 -0400

    Linear SVM working!

--- Linear_SVM_for_Facial_Recognition/svm_main.py
+++ Linear_SVM_for_Facial_Recognition/svm_main.py
@@ -1,25 +1,160 @@
 # Public libraries
 import cv2
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 from cvxopt import matrix, solvers
 
+def training(img_training, img_training_t, num_of_people):
+	rows, columns = img_training_t.shape
+
+	c = 2.0 # Linear
+
+	alpha_min = 1e-10
+
+	const_P = 1e-13
+	
+	# These variables will collect w's and b's for each class
+	svm_w = []
+	svm_b = []	
+	
+	# Column to row images
+	img_training_t = np.array(img_training_t, np.double)
+	
+	# Iterate through 40 classes
+	for class_i in range(1, num_of_people + 1, 1):
+		# Create label list where images in this class will be labeled as 1 and images not in this class will be labeled as -1
+		labels = []
+		b = 8 # b depends on ratio of training to testing data, in this case we are doing 80 : 20, so 8 out of 10 pictures are used for training and will be labeled as 1
+		# Insert non-class labels before class labels
+		labels.extend([-1] * ((class_i - 1) * 8))
+		# Insert class labels
+		labels.extend([1] * 8) # depends on ratio of training to testing data, in this case we are doing 80 : 20, so 8 out of 10 pictures are used for training and will be labeled as 1
+		# Insert non-class labels after class labels
+		labels.extend([-1] * ((num_of_people - class_i) * 8))
+		# Labels has to be of type double for quadratic programming solver
+		labels = np.array(labels, np.double)		
+
+		# Calculate K
+		K = matrix(np.zeros((columns, columns)))
+		for i in range(columns):
+			for j in range(columns):
+				# For linear SVM, single K value is calculated using dot product of two column vectors from training data
+				K[i, j] = np.dot(img_training_t[:, i], img_training_t[:, j])
+
+		# Calculate variables needed for quadratic programming solver
+	        P = matrix(np.outer(labels, labels) * K)
+		#print "\nP: \n"
+		#print P
+		q = matrix(- np.ones(columns))
+		#print "\nq: \n"
+		#print q
+
+		G = matrix(np.vstack((np.diag(-np.ones(columns)), np.identity(columns))))
+		#print "\nG: \n"
+		#print G
+		h = matrix(np.hstack((np.zeros(columns), np.ones(columns) * c)))
+		#print "\nh: \n"
+		#print h
+
+		A = matrix(labels, (1, columns))
+		#print "\nA: \n"
+		#print A
+
+		b = matrix(np.zeros(1))
+		#print "\nb: \n"
+		#print b		
+
+		
+		print "Running quadratic programming solver for class s{}...".format(class_i)
+	
+
+		# Calculate alfas using quadratic programming solver
+
+        	solution = solvers.qp(P, q, G, h, A, b)
+		alphas = np.array(solution['x'], np.double)
+		#print alphas
+
+		# Calculate w		
+		w = np.sum(alphas * labels[:, None] * img_training, axis = 0)
+		#print w
+		#print len(w)
+		svm_w.append(w)	
+		# Calculate b
+		non_zero_alpha_indexes = (np.where(alphas > alpha_min))[0]
+		#print non_zero_alphas	
+		b = []
+		#print labels
+		#print img_training
+		#print img_training[0]
+		#print non_zero_alpha_indexes
+	    	for index in non_zero_alpha_indexes: # iterate through list of indexes where alpha > const
+			b_dot_product = np.dot(img_training[index], np.transpose(w))
+			#print img_training[index]
+			#print w
+			#exit(0)
+			b_temp = labels[index] - np.dot(img_training[index], w)
+			b.append(b_temp)
+		#print "\nTHIS IS B:\n"		
+		#print b
+		bias = b[0]
+		#print bias
+		svm_b.append(bias)
+		#exit(0)
+	
+	return svm_w, svm_b
+
+def testing(img_testing, num_of_people, w, b):
+	rows, columns = img_testing.shape # every row is an image we have to test
+	#print rows
+	#print columns
+	#print b
+
+	total_correct = 0.0
+	total_incorrect = 0.0	
+
+	# For every image in testing data - check that it correctly classifies
+	for r in range(rows):
+		img = img_testing[r]
+		for i in range(num_of_people):
+			#print w[i]
+			test = np.dot(w[i], img) + b[i]
+			#print test
+			current_class = int(r / 2) + 1
+			if ((test > 0) & (i + 1 == current_class)): # True Positive - Classified Correctly
+				total_correct = total_correct + 1.0
+				print "\nImage {} classified true positive for class {}.\n".format(r + 1, current_class)
+			elif ((test <= 0) & (i + 1 != current_class)): # True Negative - Classified Correctly
+				total_correct = total_correct + 1.0
+				print "\nImage {} classified true negative for class {}.\n".format(r + 1, current_class)
+			elif ((test > 0) & (i + 1 != current_class)): # False Positive - Classified Incorrectly
+				total_incorrect = total_incorrect + 1.0
+				print "\n--Image {} classified false positive for class {}.--\n".format(r + 1, current_class)		
+			else: # False Negative - Classified Incorrectly
+				total_incorrect = total_incorrect + 1.0
+				print "\n--Image {} classified false negative as class {}.--\n".format(r + 1, current_class)		
+		
+	print "\nTotal correct classifications: {}.\n".format(total_correct)
+	print "\nTotal incorrect classifications: {}.\n".format(total_incorrect)
+	return ((total_correct / (total_correct + total_incorrect)) * 100) # Accuracy
+
 def main():
-	
+
 	# ----------------------------------------------
-	# FEATURE EXTRACTION - PCA
-	# ----------------------------------------------
+	# CREATE TRAINING AND TESTING DATA
+	# ----------------------------------------------	
 	
+	print "\nCreate Training and testing data..."	
+
 	img_height = 112
 	img_width = 92
-	num_of_pixes = img_height * img_width
+	num_of_pixels = img_height * img_width
 	num_of_people = 40
 	num_of_faces_per_person = 10
 	num_of_faces = num_of_people * num_of_faces_per_person
-	Y = [1, -1] # labels
-	X = [] # data matrix
+	img_training = [] # data matrix for training (80%) classes = [1, ..., 1, 2, ..., 40, ..., 40]
+	img_testing = [] # data matrix for testing (20%) classes = [1 , 1, 2, ..., 39, 39, 40, 40]
 	
-	# Iterate through facial images
+	# Iterate through facial images and separate them into training and testing matrixes (80 : 20 ratio) with each row being a flat version of image
 	for i in range(1, num_of_people + 1, 1):
 		for j in range(1, num_of_faces_per_person + 1, 1):
 			# Read image
@@ -27,28 +162,40 @@ def main():
 			rows, columns = img.shape
 			# 2D image matrix to 1D row vector
 			img = (np.array(img)).flatten()
-			X.append(img)
+			if j < 9:
+				img_training.append(img)
+			else:
+				img_testing.append(img)
+
+	# Convert images to type double
+	img_training = (np.array(img_training)).astype(np.double)
+	img_testing = (np.array(img_testing)).astype(np.double)
+
+	# Row image -> column images
+	img_training_t = np.transpose(img_training)	
+	#img_testing_t = np.transpose(img_testing)
+
+	# ----------------------------------------------
+	# TRAINING
+	# ----------------------------------------------
 	
-	# Calculate average / mean face
-	x_mean = (np.array(X)).mean(0)
-	cv2.imwrite('test.png', x_mean.reshape(img_height, img_width))
-	x_mean_matrix = np.array([x_mean] * num_of_faces)
-	x_mean_matrix = np.transpose(x_mean_matrix)
-	# Each face image is a column vector
-	X = np.transpose(X)
-	# Subtract mean x from each column vector x^n in X
-	F = np.subtract(X, x_mean_matrix)
-	# Calculate eigenvalues and eigenvectors - if v is eigenvector of L then Av is eigenvector of S (where S = AAt)
-	A = np.matmul(F, np.transpose(F))
-	L = np.matmul(np.transpose(A), A)
-	eigenvalues, eigenvectors = np.linalg.eig(L)
+	print "\nTraining..."
 
-	# Solve quadratic
-	alfa = cvxopt.solvers.qp(cvxopt.matrix(X), cvxopt.matrix(Y))
-	print alfa
-
+	w, b = training(img_training, img_training_t, num_of_people) # w and b are in order of class (1 through 40)
 	
+	#print w
+	#print w.shape
+	#print b
 
+	# ----------------------------------------------
+	# TESTING
+	# ----------------------------------------------
 		
+	print "\nTesting..."
+
+	accuracy = testing(img_testing, num_of_people, w, b)
+
+	print "\nOverall accuracy is {}%.\n".format(accuracy)
+
 if __name__ == "__main__":
 	main()
