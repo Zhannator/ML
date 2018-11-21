2cbb7ec75b777bbd8be3922c52f7ff40b064e368
Zhannator <zhp3@pitt.edu>
2018-11-20 19:36:23 -0500

    KNN first draft

---
+++ KNN_for_Facial_Recognition/README.md
@@ -0,0 +1,91 @@
+Support vector machines for face recognition
+
+Overview:
+
+Face recognition is implemented using KNN (1KNN in this case) with PCA and LDA techniques for dimensionality reduction. Design is validated via five-fold cross validation and average accuracy is reported for each implementation. 
+
+Dataset:
+
+The Database of Faces is used to perform all of the training and testing in this implementation. There are ten different images of each of 40 distinct subjects. The size of each image is 92x112 pixels, with 256 grey levels per pixel. The images are organised in 40 directories (one for each subject), which have names of the form sX, where X indicates the subject number (between 1 and 40). In each of these directories, there are ten different images of that subject, which have names of the form Y.pgm, where Y is the image number for that subject (between 1 and 10). This database is made available by AT&T Laboratories Cambridge and can be downloaded here: http://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html.
+
+Prerequisites:
+
+-	python 2.7
+-	cv2
+-	numpy
+-	cvxopt
+-	math
+-	random
+-	itertools
+
+Running:
+
+python knn_pca_main.py
+
+or
+
+python knn_lda_main.py
+
+
+___________________________________________________________________
+Function blocks:
+
+---	main()
+-	Reads in images
+-	Breaks images into 5 image groups for five-fold cross validation
+-	Iterates through unique combinations of 5 image groups and calls linear and polynomial svm() to train on and test on these unique combinations
+-	Reports average accuracies for linear and polynomial implementations
+
+---	svm(num_of_people, img_training, classes_training, img_testing, classes_testing, polynomial_flag = False, polynomial_degree = 2.0)
+-	Calls training() and testing() function blocks
+-	Returns accuracy
+
+---	training(img_training, classes_training, num_of_people, polynomial_flag = False, polynomial_degree = 2.0)
+-	Calculates a model (w and b) for every class using quadratic programming solver
+-	Returns a list of w’s and b’s each corresponding to one of the classes (40 people)
+
+---	testing(img_testing, classes_testing, num_of_people, w, b)
+-	Tests each test images against every class (40 people) and reports on total of correctly and incorrectly classified images
+-	Returns accuracy
+
+Results:
+
+Average linear accuracy: 84.6515625%
+
+Average quadratic (degree = 2) accuracy: 95.125%
+
+Group combinations:
+[(0, 3, 2, 4, 1), (2, 0, 3, 4, 1), (1, 3, 2, 4, 0), (1, 2, 0, 3, 4), (0, 3, 4, 2, 1), (2, 4, 1, 3, 0), (3, 4, 0, 2, 1), (0, 4, 3, 1, 2), (0, 1, 4, 3, 2), (1, 3, 4, 2, 0), (4, 2, 1, 3, 0), (2, 1, 3, 4, 0), (1, 4, 0, 2, 3), (0, 3, 2, 1, 4), (2, 0, 1, 3, 4), (0, 2, 3, 1, 4), (4, 0, 1, 3, 2), (2, 1, 0, 3, 4), (3, 1, 2, 4, 0), (3, 2, 0, 4, 1), (2, 3, 4, 0, 1), (4, 2, 0, 3, 1), (2, 4, 1, 0, 3), (2, 1, 4, 0, 3), (3, 0, 2, 4, 1), (2, 1, 0, 4, 3), (0, 2, 3, 4, 1), (4, 3, 1, 0, 2), (0, 2, 4, 3, 1), (0, 4, 2, 1, 3), (1, 0, 3, 2, 4), (1, 2, 4, 0, 3), (1, 3, 4, 0, 2), (3, 0, 1, 4, 2), (4, 1, 2, 3, 0), (4, 1, 2, 0, 3), (2, 4, 3, 1, 0), (2, 0, 4, 1, 3), (1, 4, 3, 0, 2), (2, 3, 1, 4, 0), (0, 1, 2, 3, 4), (0, 2, 1, 4, 3), (2, 3, 0, 1, 4), (4, 3, 1, 2, 0), (4, 1, 3, 0, 2), (0, 4, 1, 3, 2), (0, 2, 1, 3, 4), (0, 3, 1, 4, 2), (3, 0, 4, 2, 1), (1, 4, 2, 0, 3), (3, 1, 0, 2, 4), (1, 0, 2, 4, 3), (3, 4, 0, 1, 2), (4, 0, 3, 2, 1), (1, 4, 3, 2, 0), (0, 1, 4, 2, 3), (3, 0, 2, 1, 4), (0, 4, 2, 3, 1), (0, 2, 4, 1, 3), (2, 4, 3, 0, 1), (1, 3, 2, 0, 4), (3, 2, 0, 1, 4), (4, 1, 0, 3, 2), (2, 0, 4, 3, 1), (3, 1, 4, 2, 0), (1, 0, 3, 4, 2), (2, 0, 3, 1, 4), (0, 1, 3, 4, 2), (3, 1, 0, 4, 2), (2, 1, 4, 3, 0), (3, 2, 1, 4, 0), (0, 3, 1, 2, 4), (2, 1, 3, 0, 4), (1, 2, 3, 0, 4), (0, 1, 2, 4, 3), (1, 0, 4, 3, 2), (4, 0, 3, 1, 2), (3, 4, 2, 1, 0), (4, 3, 0, 1, 2), (4, 2, 3, 1, 0), (2, 4, 0, 1, 3), (4, 3, 2, 1, 0), (1, 2, 0, 4, 3), (1, 0, 4, 2, 3), (1, 4, 0, 3, 2), (4, 2, 3, 0, 1), (0, 4, 1, 2, 3), (1, 0, 2, 3, 4), (4, 3, 2, 0, 1), (4, 2, 1, 0, 3), (3, 0, 4, 1, 2), (3, 1, 4, 0, 2), (2, 0, 1, 4, 3), (3, 2, 4, 1, 0), (2, 3, 4, 1, 0), (4, 3, 0, 2, 1), (0, 1, 3, 2, 4), (1, 2, 4, 3, 0), (2, 3, 1, 0, 4), (4, 2, 0, 1, 3), (4, 0, 2, 3, 1), (0, 3, 4, 1, 2), (1, 3, 0, 2, 4), (4, 1, 0, 2, 3), (2, 4, 0, 3, 1), (3, 4, 1, 0, 2), (3, 4, 1, 2, 0), (1, 4, 2, 3, 0), (3, 4, 2, 0, 1), (4, 0, 1, 2, 3), (3, 0, 1, 2, 4), (4, 1, 3, 2, 0), (0, 4, 3, 2, 1), (3, 2, 1, 0, 4), (1, 2, 3, 4, 0), (3, 2, 4, 0, 1), (1, 3, 0, 4, 2), (4, 0, 2, 1, 3), (3, 1, 2, 0, 4), (2, 3, 0, 4, 1)]
+
+All linear accuracies (1 per group combination):
+[85.      83.53125 82.875   81.78125 85.      84.375   88.1875  85.21875
+ 85.21875 82.875   85.9375  84.375   80.875   84.8125  83.78125 84.8125
+ 85.21875 83.78125 86.375   88.1875  83.53125 85.78125 84.6875  84.6875
+ 88.1875  84.6875  85.      85.21875 85.      85.09375 81.78125 80.875
+ 82.      85.90625 85.9375  85.71875 84.375   84.6875  82.      84.375
+ 84.8125  85.09375 83.78125 85.9375  85.21875 85.21875 84.8125  85.21875
+ 88.1875  80.875   85.875   80.875   85.90625 85.78125 82.875   85.09375
+ 85.875   85.      85.09375 83.53125 81.78125 85.875   85.21875 83.53125
+ 86.375   82.      83.78125 85.21875 85.90625 84.375   86.375   84.8125
+ 83.78125 81.78125 85.09375 82.      85.21875 86.375   85.21875 85.9375
+ 84.6875  85.9375  80.875   80.875   82.      85.78125 85.09375 81.78125
+ 85.78125 85.71875 85.90625 85.90625 84.6875  86.375   84.375   85.78125
+ 84.8125  82.875   83.78125 85.71875 85.78125 85.21875 81.78125 85.71875
+ 83.53125 85.90625 86.375   82.875   88.1875  85.71875 85.875   85.9375
+ 85.      85.875   82.875   88.1875  82.      85.71875 85.875   83.53125]
+
+All quadratic accuracies (1 per group combination):
+[95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125
+ 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125 95.125]
+ 
+ ___________________________________________________________________
---
+++ KNN_for_Facial_Recognition/knn_pca_main.py
@@ -0,0 +1,278 @@
+# Public libraries
+import cv2
+import numpy as np
+from cvxopt import matrix, solvers
+import math
+import random
+import itertools
+
+def training(img_training, classes_training, num_of_people, polynomial_flag = False, polynomial_degree = 2.0):
+	# Row image -> column images
+	img_training_t = np.transpose(img_training)	
+	rows, columns = img_training_t.shape
+
+	# Constants
+	c = 2.0
+
+	# These variables will collect w's and b's for each class
+	svm_w = np.zeros((num_of_people, rows))
+	svm_b = np.zeros(num_of_people)	
+	
+	# Column to row images
+	img_training_t = np.array(img_training_t, np.double)
+	
+	# Iterate through 40 classes
+	for class_i in range(1, num_of_people + 1, 1):
+		# Create label list where images in this class will be labeled as 1 and images not in this class will be labeled as -1
+		labels = np.zeros(columns)
+		labels_index = 0
+		for my_c in classes_training:
+			if my_c == class_i:
+				labels[labels_index] = 1
+			else:
+				labels[labels_index] = -1
+			labels_index = labels_index + 1
+
+		# Labels has to be of type double for quadratic programming solver
+		labels = np.array(labels, np.double)		
+
+		# Calculate K
+		K = matrix(np.zeros((columns, columns)))
+		for i in range(columns):
+			for j in range(columns):
+				# Choose between linear and polynomial svm
+				if polynomial_flag == True:
+					K[i, j] = math.pow((np.dot(img_training_t[:, i], img_training_t[:, j]) + 1.0), polynomial_degree)
+				else: # Linear and aslo default
+					# For linear SVM, single K value is calculated using dot product of two column vectors from training data			
+					K[i, j] = np.dot(img_training_t[:, i], img_training_t[:, j])
+				
+				
+
+		# Calculate variables needed for quadratic programming solver
+	        P = matrix(np.outer(labels, labels) * K)
+		q = matrix(- np.ones(columns))
+		G = matrix(np.vstack((np.diag(-np.ones(columns)), np.identity(columns))))
+		h = matrix(np.hstack((np.zeros(columns), np.ones(columns) * c)))
+		A = matrix(labels, (1, columns))
+		b = matrix(np.zeros(1))		
+		
+		print "Running quadratic programming solver for class s{}...".format(class_i)
+	
+		# Calculate alfas using quadratic programming solver
+		solvers.options['show_progress'] = False        	
+		solution = solvers.qp(P, q, G, h, A, b)
+		alphas = np.array(solution['x'], np.double)		
+		alpha_min = min(alphas)
+
+		# Calculate w		
+		w = np.sum(alphas * labels[:, None] * img_training, axis = 0)
+		svm_w[class_i - 1] = w	
+		# Calculate b
+		non_zero_alpha_indexes = (np.where(alphas > alpha_min))[0]
+		b = np.zeros(len(non_zero_alpha_indexes))
+		b_index = 0
+	    	for index in non_zero_alpha_indexes: # iterate through list of indexes where alpha > const
+			b_dot_product = np.dot(img_training[index], np.transpose(w))
+			b_temp = labels[index] - np.dot(img_training[index], w)
+			b[b_index] = b_temp
+			b_index = b_index + 1
+		bias = b[0]
+		svm_b[class_i - 1] = bias
+	
+	return svm_w, svm_b
+
+def testing(img_testing, classes_testing, num_of_people, w, b):
+	rows, columns = img_testing.shape # every row is an image we have to test
+
+	total_correct = 0.0
+	total_incorrect = 0.0
+
+	# For every image in testing data - check that it correctly classifies
+	for r in range(rows):
+		img = img_testing[r]
+		for i in range(num_of_people):
+			test = np.dot(w[i], img) + b[i]
+			current_class = classes_testing[r]
+			if ((test > 0) & (i + 1 == current_class)): # True Positive - Classified Correctly
+				total_correct = total_correct + 1.0
+				#print "\nImage {} classified true positive for class {}.\n".format(r + 1, current_class)
+			elif ((test <= 0) & (i + 1 != current_class)): # True Negative - Classified Correctly
+				total_correct = total_correct + 1.0
+				#print "\nImage {} classified true negative for class {}.\n".format(r + 1, current_class)
+			elif ((test > 0) & (i + 1 != current_class)): # False Positive - Classified Incorrectly
+				total_incorrect = total_incorrect + 1.0
+				#print "\n--Image {} classified false positive for class {}.--\n".format(r + 1, current_class)		
+			else: # False Negative - Classified Incorrectly
+				total_incorrect = total_incorrect + 1.0
+				#print "\n--Image {} classified false negative as class {}.--\n".format(r + 1, current_class)		
+		
+	print "\nTotal correct classifications: {}.\n".format(total_correct)
+	print "\nTotal incorrect classifications: {}.\n".format(total_incorrect)
+	accuracy = ((total_correct / (total_correct + total_incorrect)) * 100) # Accuracy
+	print "\nAccuracy: {}%.\n".format(accuracy)
+	return accuracy
+
+def svm(num_of_people, img_training, classes_training, img_testing, classes_testing, polynomial_flag = False, polynomial_degree = 2.0):
+	# ----------------------------------------------
+	# TRAINING
+	# ----------------------------------------------
+	
+	print "\nTraining..."
+
+	w, b = training(img_training, classes_training, num_of_people, polynomial_flag, polynomial_degree) # w and b are in order of class (1 through 40)
+
+	# ----------------------------------------------
+	# TESTING
+	# ----------------------------------------------
+		
+	print "\nTesting..."
+
+	accuracy = testing(img_testing, classes_testing, num_of_people, w, b)
+	
+	return accuracy
+
+def faces_to_eigenfaces(faces):
+	# row faces to column faces -  each face is a column vector
+	X = np.transpose(faces)
+	
+	# Calculate mean face
+	X_mean = X.mean(1)
+	
+	# Subtract mean from each column vector in 
+	A = X - X_mean
+	
+	# Calculate convergence matrix
+	AtA = np.matmul(np.transpose(A), A)
+	
+	# Calculate eigenvalues and eigenfaces of AtA
+	eigenvalues, eigenfaces = np.linalg.eig(AtA)
+	
+	return eigenvalues, eigenfaces
+	
+def main():
+
+	# ----------------------------------------------
+	# READ IN DATA
+	# ----------------------------------------------	
+	
+	print "\nCreate Training and testing data..."	
+
+	img_height = 112
+	img_width = 92
+	num_of_pixels = img_height * img_width
+	num_of_people = 40
+	num_of_faces_per_person = 10
+	num_of_faces = num_of_people * num_of_faces_per_person
+	images = np.zeros((num_of_faces, num_of_pixels)) # all images, each as row vector
+	classes = np.zeros(num_of_faces) # all classes
+
+	# Iterate through facial images and separate them into training and testing matrixes (80 : 20 ratio) with each row being a flat version of image
+	counter = 0
+	for i in range(1, num_of_people + 1, 1):
+		for j in range(1, num_of_faces_per_person + 1, 1):
+			# Read image
+			img = cv2.imread("data/s" + str(i) + "/" + str(j) + ".pgm", 0)
+			rows, columns = img.shape
+			# 2D image matrix to 1D row vector
+			images[counter] = (np.array(img)).flatten()
+			classes[counter] = i
+			counter = counter + 1
+
+
+	# Convert images to type double
+	images = images.astype(np.double)
+	
+	# Convert image matrix to eigenfaces
+	eigenvalues, eigenfaces = faces_to_eigenfaces(images)
+	
+	# ----------------------------------------------
+	# BEGIN FIVE-FOLD CROSS VALIDATION PROCEDURE
+	# ----------------------------------------------
+
+	print "\nCreate Training and testing data groupd for 5-fold cross validation..."		
+	
+	# Shuffle images into 5 equal-size groups with equal number of images from each class
+	img_group_0 = np.zeros((num_of_faces / 5, num_of_pixels))
+	img_group_1 = np.zeros((num_of_faces / 5, num_of_pixels))
+	img_group_2 = np.zeros((num_of_faces / 5, num_of_pixels))
+	img_group_3 = np.zeros((num_of_faces / 5, num_of_pixels))
+	img_group_4 = np.zeros((num_of_faces / 5, num_of_pixels))
+	class_group_0 = np.zeros(num_of_faces / 5)
+	class_group_1 = np.zeros(num_of_faces / 5)
+	class_group_2 = np.zeros(num_of_faces / 5)
+	class_group_3 = np.zeros(num_of_faces / 5)
+	class_group_4 = np.zeros(num_of_faces / 5)
+	counter = 0	
+	for i in range(num_of_people):
+		rand_indexes = random.sample(range(0, 10), 10)
+		# group 0
+		img_group_0[counter] = images[i*10 + rand_indexes[0]]
+		img_group_0[counter + 1] = images[i*10 + rand_indexes[1]]
+		class_group_0[counter] = i + 1
+		class_group_0[counter + 1] = i + 1
+		# group 1
+		img_group_1[counter] = images[i*10 + rand_indexes[2]]
+		img_group_1[counter + 1] = images[i*10 + rand_indexes[3]]
+		class_group_1[counter] = i + 1
+		class_group_1[counter + 1] = i + 1
+		# group 2
+		img_group_2[counter] = images[i*10 + rand_indexes[4]]
+		img_group_2[counter + 1] = images[i*10 + rand_indexes[5]]
+		class_group_2[counter] = i + 1
+		class_group_2[counter + 1] = i + 1
+		# group 3
+		img_group_3[counter] = images[i*10 + rand_indexes[6]]
+		img_group_3[counter + 1] = images[i*10 + rand_indexes[7]]
+		class_group_3[counter] = i + 1
+		class_group_3[counter + 1] = i + 1
+		# group 4
+		img_group_4[counter] = images[i*10 + rand_indexes[8]]
+		img_group_4[counter + 1] = images[i*10 + rand_indexes[9]]
+		class_group_4[counter] = i + 1
+		class_group_4[counter + 1] = i + 1
+		counter = counter + 2
+
+	image_groups = [img_group_0, img_group_1, img_group_2, img_group_3, img_group_4]
+	image_classes_groups = [class_group_0, class_group_1, class_group_2, class_group_3, class_group_4]
+
+	print "\nRunning SVMs..."		
+	
+	# Use different unique combinations of the 5 groups to perform training and testing - average accuracy
+	combinations = set(itertools.permutations([0, 1, 2, 3, 4]))
+	average_accuracy_linear = 0
+	accuracies_linear = np.zeros(120)
+	average_accuracy_quadratic = 0
+	accuracies_quadratic = np.zeros(120)
+	counter = 0
+	for combo in combinations:
+		print "\n----------------------------------------------------------------------"
+		print "Running combination {}. This is {} out of {}.".format(combo, counter + 1, 120)
+		print "----------------------------------------------------------------------\n"
+		# Training data
+		img_training = np.concatenate((image_groups[combo[0]], image_groups[combo[1]], image_groups[combo[2]], image_groups[combo[3]]))
+		classes_training = np.concatenate((image_classes_groups[combo[0]], image_classes_groups[combo[1]], image_classes_groups[combo[2]], image_classes_groups[combo[3]]))
+		# Testing data
+		img_testing = np.array(image_groups[combo[4]], np.double)
+		classes_testing = np.array(image_classes_groups[combo[4]])
+		# Run linear SVM		
+		print "\nLinear SVM...\n"		
+		accuracies_linear[counter] = svm(num_of_people, img_training, classes_training, img_testing, classes_testing)
+		# Run quadratic (degree = 2) SVM
+		print "\nQuadratic SVM...\n"
+		accuracies_quadratic[counter] = svm(num_of_people, img_training, classes_training, img_testing, classes_testing, True, 2.0)
+		counter = counter + 1	
+	
+	print "\nCombinations:\n"
+	print combinations 
+	print "\nLinear accuracies:\n"
+	print accuracies_linear
+	print "\nQuadratic accuracies:\n"
+	print accuracies_quadratic
+	average_accuracy_linear = sum(accuracies_linear) / 120
+	print "\nAverage linear accuracy: {}%.\n".format(average_accuracy_linear)
+	average_accuracy_quadratic = sum(accuracies_quadratic) / 120
+	print "\nAverage quadratic accuracy: {}%.\n".format(average_accuracy_quadratic)
+
+if __name__ == "__main__":
+	main()
